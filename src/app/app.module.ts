77911732133c1514899553236750a9c711e007ea
bloodcv <554365000@qq.com>
2019-12-23 14:02:25 +0800

    [saya]引入antUI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
+
+/** 配置 angular i18n **/
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+registerLocaleData(zh);
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -18,6 +28,7 @@ import { WatchParentComponent } from './components/index/studentMg/watch-parent/
 // teacherMg
 import { TeacherMgComponent } from './components/index/teacherMg/teacher-mg/teacher-mg.component';
 import { AddTeacherComponent } from './components/index/teacherMg/add-teacher/add-teacher.component';
+import { IconsProviderModule } from './icons-provider.module';
 
 @NgModule({
   declarations: [
@@ -37,9 +48,18 @@ import { AddTeacherComponent } from './components/index/teacherMg/add-teacher/ad
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    /** 导入 ng-zorro-antd 模块 **/
+    NgZorroAntdModule,
+    IconsProviderModule
   ],
-  providers: [],
+  /** 配置 ng-zorro-antd 国际化（文案 及 日期） **/
+  providers   : [
+    { provide: NZ_I18N, useValue: zh_CN }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
